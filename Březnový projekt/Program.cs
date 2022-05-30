6e1f75618dfb1677c51b78b8aeb43fbb5c0e89c8
Petr Baťha <petr.batha@educhem.cz>
2022-05-30 08:38:09 +0200

    Ano

--- Březnový projekt/Program.cs
+++ Březnový projekt/Program.cs
@@ -5,12 +5,7 @@ int a = Convert.ToInt32(cislo);
 Console.WriteLine("Zadejte druhý číslo");
 string cislo2 = Console.ReadLine();
 int b = Convert.ToInt32(cislo2);
-if (a == b)
-{
-    Console.WriteLine("cisla se schodují");
-}
-else
-{
+
     if (a > b)
     {
         Console.WriteLine(cislo);
@@ -21,4 +16,4 @@ else
         Console.WriteLine(cislo2);
         Console.WriteLine(cislo);
     }
-}
+
